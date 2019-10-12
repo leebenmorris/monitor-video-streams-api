0854930b180d31708b7ff6b850e10f68f930f2fa
Lee Morris <lee.morris@bbc.co.uk>
2019-10-12 17:05:41 +0100

    refactor, investigate events

--- public/index.js
+++ public/index.js
@@ -1,11 +1,15 @@
 /* global document, YT */
 
-document.querySelectorAll('iframe').forEach((element, i) => {
-    element.setAttribute('id', `player_${i}`);
-    const src = element.getAttribute('src');
-    element.setAttribute('src', `${src}?enablejsapi=1`);
-});
+const eventsMap = {
+    '-1': 'notStarted',
+    '0': 'ended',
+    '1': 'playing',
+    '2': 'paused',
+    '3': 'buffering',
+    '5': 'cued',
+};
 
+// load the YouTube IFrame Player API code asynchronously
 const tag = document.createElement('script');
 const firstScriptTag = document.getElementsByTagName('script')[0];
 
@@ -13,21 +17,29 @@ tag.src = 'https://www.youtube.com/iframe_api';
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function onPlayerReady(event) {
-    // console.log('onPlayerReady', { eventTarget: event.target, eventData: event.data });
+    console.log('onPlayerReady', {
+        [event.target.a.id]: eventsMap[event.data],
+        eventTarget: event.target,
+    });
     event.target.playVideo();
 }
 
 function onPlayerStateChange(event) {
-    console.log('onPlayerStateChange', { eventTarget: event.target, eventData: event.data });
+    console.log('onPlayerStateChange', {
+        [event.target.a.id]: eventsMap[event.data],
+        eventTime: Date.now(),
+    });
 }
 
-// Creates an <iframe> (and YouTube player) after the API code downloads.
 // eslint-disable-next-line no-unused-vars
 function onYouTubeIframeAPIReady() {
     document.querySelectorAll('iframe').forEach((element, i) => {
-        const id = `player_${i}`;
+        // modify iframe elements to make them compatible with the YouTube IFrame Player API
+        const id = `video_${i}`;
+        const src = element.getAttribute('src');
 
         element.setAttribute('id', id);
+        element.setAttribute('src', `${src}?enablejsapi=1`);
 
         // eslint-disable-next-line no-new
         new YT.Player(id, {
