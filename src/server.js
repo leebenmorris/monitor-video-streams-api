c35040622c79349ad1b943b3be3f2aeae4e571fb
Lee Morris <lee.morris@bbc.co.uk>
2019-10-12 20:08:39 +0100

    websocket experiments

--- src/server.js
+++ src/server.js
@@ -1,9 +1,36 @@
+const http = require('http');
+
 const express = require('express');
+const WebSocket = require('ws');
 const cors = require('cors');
 
 const app = express();
+const server = http.Server(app);
+const socket = new WebSocket.Server({ server, clientTracking: true });
 
 app.use(cors());
 app.use(express.static(`${__dirname}/../public`));
 
-app.listen(8000);
+const events = {};
+
+socket.on('connection', (client) => {
+    client.on('message', (message) => {
+        Object.entries(JSON.parse(message)).forEach(([key, value]) => {
+            // eslint-disable-next-line no-unused-expressions
+            events[key] ? Object.assign(events[key], value) : (events[key] = value);
+        });
+        // console.log(events);
+        const player_0Playing =
+            events.player_0 && (events.player_0.playing || 0) > (events.player_0.paused || 0);
+        const player_1Playing =
+            events.player_1 && (events.player_1.playing || 0) > (events.player_1.paused || 0);
+        if (player_0Playing && player_1Playing) {
+            const sendMessage = {
+                player_0: 'pauseVideo',
+            };
+            client.send(JSON.stringify(sendMessage));
+        }
+    });
+});
+
+server.listen(8000);
