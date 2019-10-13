d84c9ff61dea9398a426c7eacda7987f122b6422
Lee Morris <lee.morris@bbc.co.uk>
2019-10-13 19:59:15 +0100

    modify tests to reflect new API and test that correct player gets paused

--- src/serverSetup.int.test.js
+++ src/serverSetup.int.test.js
@@ -12,8 +12,13 @@ const PORT = 1234;
 describe('server', function() {
     let server;
     let socket;
+    // let consoleLog;
 
     beforeEach(function(done) {
+        // suppress console.log messages elsewhere in the code
+        consoleLog = console.log;
+        console.log = () => {};
+
         server = serverSetup.listen(PORT);
         socket = io(`http://localhost:${PORT}`)
             .on('connect_error', done)
@@ -21,6 +26,9 @@ describe('server', function() {
     });
 
     afterEach(function(done) {
+        // restore console.log
+        console.log = consoleLog;
+
         socket.close();
         server.close();
         done();
@@ -31,22 +39,29 @@ describe('server', function() {
     });
 
     it('should respond with a control message if more than three status messages are sent', function(done) {
-        const expectedControlMessage = {
-            player1: 'pauseVideo',
-        };
+        let messageCount = 0;
+        const expectedMessages = [['player1', 'pauseVideo'], ['player2', 'pauseVideo']];
 
         socket
-            .once('playerControl', (message) => {
+            .on('playerControl', (message) => {
+                const expectedMessage = expectedMessages[messageCount];
+                messageCount += 1;
+
+                console.log({ messageInTest: message });
+
                 try {
-                    message.should.deep.equal(expectedControlMessage);
-                    done();
+                    message.should.deep.equal(expectedMessage);
+                    if (messageCount === expectedMessages.length) {
+                        done();
+                    }
                 } catch (err) {
                     done(err);
                 }
             })
-            .emit('playerStatus', { player1: 'playing' })
-            .emit('playerStatus', { player2: 'playing' })
-            .emit('playerStatus', { player3: 'playing' })
-            .emit('playerStatus', { player4: 'playing' });
+            .emit('playerStatus', ['player1', 'playing'])
+            .emit('playerStatus', ['player2', 'playing'])
+            .emit('playerStatus', ['player3', 'playing'])
+            .emit('playerStatus', ['player4', 'playing'])
+            .emit('playerStatus', ['player5', 'playing']);
     });
 });
