ec79ac941f81bf280a1d50c6265622242c949cf8
Lee Morris <lee.morris@bbc.co.uk>
2019-10-13 12:03:07 +0100

    swap out ws for socket.io socket.io handles disconnects and reconnects automatically.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
 const http = require('http');
 
 const express = require('express');
-const WebSocket = require('ws');
-const cors = require('cors');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.Server(app);
-const socket = new WebSocket.Server({ server, clientTracking: true });
-
-app.use(cors());
-app.use(express.static(`${__dirname}/../public`));
+const io = socketIO(server);
 
 const maxPlaying = 3;
 
@@ -17,20 +13,10 @@ const events = {};
 
 const nowPlaying = new Set();
 
-function noop() {}
+app.use(express.static(`${__dirname}/../public`));
 
-function heartbeat() {
-    this.isAlive = true;
-}
-
-socket.on('connection', (client) => {
-    // eslint-disable-next-line no-param-reassign
-    client.isAlive = true;
-    client.on('pong', heartbeat);
-
-    client.on('message', (rawMessage) => {
-        const message = JSON.parse(rawMessage);
-
+io.on('connection', (socket) => {
+    socket.on('message', (message) => {
         Object.entries(message).forEach(([key, value]) => {
             // eslint-disable-next-line no-unused-expressions
             events[key] ? Object.assign(events[key], value) : (events[key] = value);
@@ -54,32 +40,9 @@ socket.on('connection', (client) => {
             const sendMessage = {
                 [[...nowPlaying][0]]: 'pauseVideo',
             };
-            client.send(JSON.stringify(sendMessage));
+            socket.emit('message', sendMessage);
         }
-
-        // const player_0Playing =
-        //     events.player_0 && (events.player_0.playing || 0) > (events.player_0.paused || 0);
-        // const player_1Playing =
-        //     events.player_1 && (events.player_1.playing || 0) > (events.player_1.paused || 0);
-        // if (player_0Playing && player_1Playing) {
-        // const sendMessage = {
-        //     player_0: 'pauseVideo',
-        // };
-        // client.send(JSON.stringify(sendMessage));
-        // }
     });
 });
-
-setInterval(function ping() {
-    socket.clients.forEach(function each(client) {
-        if (client.isAlive === false) {
-            return client.terminate();
-        }
-
-        // eslint-disable-next-line no-param-reassign
-        client.isAlive = false;
-        client.ping(noop);
-    });
-}, 1000);
 
 server.listen(8000);
