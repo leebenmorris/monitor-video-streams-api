262a985eb460dbbc9ae0895580d1df82c6143faf
Lee Morris <lee.morris@bbc.co.uk>
2019-10-13 10:02:23 +0100

    send pause command when playing mroe than 3 videos, try to handle socket disconnects

--- src/server.js
+++ src/server.js
@@ -11,26 +11,75 @@ const socket = new WebSocket.Server({ server, clientTracking: true });
 app.use(cors());
 app.use(express.static(`${__dirname}/../public`));
 
+const maxPlaying = 3;
+
 const events = {};
 
+const nowPlaying = new Set();
+
+function noop() {}
+
+function heartbeat() {
+    this.isAlive = true;
+}
+
 socket.on('connection', (client) => {
-    client.on('message', (message) => {
-        Object.entries(JSON.parse(message)).forEach(([key, value]) => {
+    // eslint-disable-next-line no-param-reassign
+    client.isAlive = true;
+    client.on('pong', heartbeat);
+
+    client.on('message', (rawMessage) => {
+        const message = JSON.parse(rawMessage);
+
+        Object.entries(message).forEach(([key, value]) => {
             // eslint-disable-next-line no-unused-expressions
             events[key] ? Object.assign(events[key], value) : (events[key] = value);
+
+            const playerState = Object.keys(value)[0];
+
+            if (playerState === 'playing') {
+                nowPlaying.add(key);
+            } else {
+                nowPlaying.delete(key);
+            }
         });
-        // console.log(events);
-        const player_0Playing =
-            events.player_0 && (events.player_0.playing || 0) > (events.player_0.paused || 0);
-        const player_1Playing =
-            events.player_1 && (events.player_1.playing || 0) > (events.player_1.paused || 0);
-        if (player_0Playing && player_1Playing) {
+
+        console.log({
+            events,
+            nowPlaying: [...nowPlaying],
+            nowPlayingFirst: [...nowPlaying][0],
+        });
+
+        if (nowPlaying.size > maxPlaying) {
             const sendMessage = {
-                player_0: 'pauseVideo',
+                [[...nowPlaying][0]]: 'pauseVideo',
             };
             client.send(JSON.stringify(sendMessage));
         }
+
+        // const player_0Playing =
+        //     events.player_0 && (events.player_0.playing || 0) > (events.player_0.paused || 0);
+        // const player_1Playing =
+        //     events.player_1 && (events.player_1.playing || 0) > (events.player_1.paused || 0);
+        // if (player_0Playing && player_1Playing) {
+        // const sendMessage = {
+        //     player_0: 'pauseVideo',
+        // };
+        // client.send(JSON.stringify(sendMessage));
+        // }
     });
 });
+
+setInterval(function ping() {
+    socket.clients.forEach(function each(client) {
+        if (client.isAlive === false) {
+            return client.terminate();
+        }
+
+        // eslint-disable-next-line no-param-reassign
+        client.isAlive = false;
+        client.ping(noop);
+    });
+}, 1000);
 
 server.listen(8000);
