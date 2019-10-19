7856e6a21713c7bd2b9f875efde4d5dd0d721525
Lee Morris <lee.morris@bbc.co.uk>
2019-10-19 10:49:26 +0100

    move business logic out of onYouTubeIframeAPIReady again

--- public/index.js
+++ public/index.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-console */
 /* global YT, io */
 
+const tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 const ioServerUrl = 'https://monitor-video-streams-api.herokuapp.com';
 
 const players = {};
@@ -16,62 +22,57 @@ const mapEventNumToName = {
     '5': 'cued',
 };
 
-const tag = document.createElement('script');
+const socket = io(ioServerUrl)
+    .on('error', (error) => {
+        console.log(`socket.io Error: ${JSON.stringify(error, null, 4)}`);
+    })
+    .on('connect_error', (error) => {
+        console.log(`socket.io Connection Error: ${JSON.stringify(error, null, 4)}`);
+    })
+    .on('connect', () => {
+        Object.entries(disconnectedPlayerStates).forEach(([id, state]) => {
+            if (state === 'playing') {
+                players[id].playVideo();
+            }
+        });
+        console.log('Connected to:', socket.id);
+    })
+    .on('playerControl', ([id, command]) => {
+        players[id][command]();
+        console.log('playerControl received:', [id, command]);
+    })
+    .on('disconnect', () => {
+        Object.entries(players).forEach(([id, player]) => {
+            const currentPlayerState = mapEventNumToName[player.getPlayerState()];
+            disconnectedPlayerStates[id] = currentPlayerState;
+            player.pauseVideo();
+        });
+        console.log(`Disconnected from socket.io server, so all videos paused`);
+    });
 
-tag.src = 'https://www.youtube.com/iframe_api';
-const firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function onPlayerReady(event) {
+    const message = [event.target.a.id, 'ready'];
+
+    console.log('onPlayerReady', message);
+    socket.emit('playerStatus', message);
+}
+
+function onPlayerStateChange(event) {
+    if (socket.disconnected) {
+        event.target.pauseVideo();
+    }
+
+    const message = [event.target.a.id, mapEventNumToName[event.data]];
+
+    console.log('onPlayerStateChange', message);
+    socket.emit('playerStatus', message);
+}
 
 // eslint-disable-next-line no-unused-vars
 window.onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
     console.log('onYouTubeIframeAPIReady called');
 
     // setup the socket.io connection and attach event listeners
-    const socket = io(ioServerUrl)
-        .on('error', (error) => {
-            console.log(`socket.io Error: ${JSON.stringify(error, null, 4)}`);
-        })
-        .on('connect_error', (error) => {
-            console.log(`socket.io Connection Error: ${JSON.stringify(error, null, 4)}`);
-        })
-        .on('connect', () => {
-            Object.entries(disconnectedPlayerStates).forEach(([id, state]) => {
-                if (state === 'playing') {
-                    players[id].playVideo();
-                }
-            });
-            console.log('Connected to:', socket.id);
-        })
-        .on('playerControl', ([id, command]) => {
-            players[id][command]();
-            console.log('playerControl received:', [id, command]);
-        })
-        .on('disconnect', () => {
-            Object.entries(players).forEach(([id, player]) => {
-                const currentPlayerState = mapEventNumToName[player.getPlayerState()];
-                disconnectedPlayerStates[id] = currentPlayerState;
-                player.pauseVideo();
-            });
-            console.log(`Disconnected from socket.io server, so all videos paused`);
-        });
-
-    function onPlayerReady(event) {
-        const message = [event.target.a.id, 'ready'];
-
-        console.log('onPlayerReady', message);
-        socket.emit('playerStatus', message);
-    }
-
-    function onPlayerStateChange(event) {
-        if (socket.disconnected) {
-            event.target.pauseVideo();
-        }
-
-        const message = [event.target.a.id, mapEventNumToName[event.data]];
-
-        console.log('onPlayerStateChange', message);
-        socket.emit('playerStatus', message);
-    }
 
     document.querySelectorAll('iframe').forEach((element, i) => {
         // modify iFrame elements to make them compatible with the YouTube IFrame Player API.
