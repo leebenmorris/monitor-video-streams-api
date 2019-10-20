6b8bf55406e0a0e3d2bd4bc8e399869167f93389
Lee Morris <lee.morris@bbc.co.uk>
2019-10-20 16:49:58 +0100

    server video list from server

--- src/serverSetup.js
+++ src/serverSetup.js
@@ -11,11 +11,41 @@ const maxPlaying = 3;
 
 const nowPlaying = new Set();
 
-app.use(express.static(`${__dirname}/../public`));
+const videoList = [
+    {
+        videoId: 'hY7m5jjJ9mM',
+        height: 158,
+        width: 280,
+    },
+    {
+        videoId: 'F7uSppqT8bM',
+        height: 158,
+        width: 280,
+    },
+    {
+        videoId: 'rNSnfXl1ZjU',
+        height: 158,
+        width: 280,
+    },
+    {
+        videoId: '94PLgLKcGW8',
+        height: 158,
+        width: 280,
+    },
+];
+
+// frontend served from /docs directory to suit github pages
+app.use(express.static(`${__dirname}/../docs`));
 
 io.on('connection', (socket) => {
     // eslint-disable-next-line no-console
     console.log(`Socket connected to ${socket.id}`);
+
+    socket.on('getVideoList', (cb) => {
+        console.log('getVideoList called');
+        cb(videoList);
+    });
+
     socket.on('playerStatus', ([player, status]) => {
         if (status === 'playing') {
             nowPlaying.add(player);
